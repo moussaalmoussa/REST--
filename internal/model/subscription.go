b40af51e78e7f342f7590a2a9a2b764beb566769
Moussa Al Moussa <68187784+moussaalmoussa@users.noreply.github.com>
2026-05-06 12:39:10 +0300

    Update subscription.go

--- internal/model/subscription.go
+++ internal/model/subscription.go
@@ -1,1 +1,9 @@
-
+// model/subscription.go
+type Subscription struct {
+    ID          string  `json:"id" db:"id"`
+    ServiceName string  `json:"service_name" db:"service_name"`
+    Price       int     `json:"price" db:"price"`
+    UserID      string  `json:"user_id" db:"user_id"`
+    StartDate   string  `json:"start_date" db:"start_date"`   // формат "MM-YYYY"
+    EndDate     *string `json:"end_date,omitempty" db:"end_date"`
+}
