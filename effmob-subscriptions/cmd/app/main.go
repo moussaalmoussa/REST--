0fff5d68252a035bd335f2ce4eb7953b420ef0e9
Moussa Al Moussa <68187784+moussaalmoussa@users.noreply.github.com>
2026-05-06 12:33:04 +0300

    main.go

--- effmob-subscriptions/cmd/app/main.go
+++ effmob-subscriptions/cmd/app/main.go
@@ -1,1 +1,39 @@
+package main
 
+import (
+    "effmob-subscriptions/internal/config"
+    "effmob-subscriptions/internal/database"
+    "effmob-subscriptions/internal/handler"
+    "effmob-subscriptions/internal/logger"
+    "github.com/gorilla/mux"
+    "net/http"
+    "os"
+)
+
+// @title          Subscription Aggregator API
+// @version        1.0
+// @description    Сервис управления подписками пользователей.
+
+// @host           localhost:8080
+// @BasePath       /
+
+func main() {
+    log := logger.New()
+    cfg, err := config.Load()
+    if err != nil {
+        log.Fatalf("config error: %v", err)
+    }
+    db, err := database.Connect(cfg.DB)
+    if err != nil {
+        log.Fatalf("db connection: %v", err)
+    }
+    if err := database.RunMigrations(db); err != nil {
+        log.Fatalf("migrations: %v", err)
+    }
+    r := mux.NewRouter()
+    handler.RegisterHandlers(r, db, log)
+    log.Infof("starting server on %s", cfg.ServerAddr)
+    if err := http.ListenAndServe(cfg.ServerAddr, r); err != nil {
+        log.Fatal(err)
+    }
+}
