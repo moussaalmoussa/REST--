9f3dd302e18a44173808a482e27b02ffb6fd85c1
Moussa Al Moussa <68187784+moussaalmoussa@users.noreply.github.com>
2026-05-06 12:39:57 +0300

    Update 001_init.up.sql

--- internal/database/migrations/001_init.up.sql
+++ internal/database/migrations/001_init.up.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE IF NOT EXISTS subscriptions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    service_name VARCHAR(255) NOT NULL,
+    price INTEGER NOT NULL CHECK (price > 0),
+    user_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE,
+    created_at TIMESTAMP DEFAULT now()
+);
