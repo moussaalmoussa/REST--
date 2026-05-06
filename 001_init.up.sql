7dda8dcb80cb10bfab2cfd115c9f93e16746e9b1
Moussa Al Moussa <68187784+moussaalmoussa@users.noreply.github.com>
2026-05-06 12:40:20 +0300

    Delete 001_init.up.sql

--- 001_init.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS subscriptions (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    service_name VARCHAR(255) NOT NULL,
-    price INTEGER NOT NULL CHECK (price > 0),
-    user_id UUID NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE,
-    created_at TIMESTAMP DEFAULT now()
-);
