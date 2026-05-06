30a14a402eb2c68ec5dc3b2830bba3a0a1e49a5d
Moussa Al Moussa <68187784+moussaalmoussa@users.noreply.github.com>
2026-05-06 12:42:58 +0300

    Update subscription.go

--- internal/handler/subscription.go
+++ internal/handler/subscription.go
@@ -1,1 +1,37 @@
+package handler
 
+import (
+    "database/sql"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    // другие импорты (uuid, ваш логгер, model...)
+)
+
+type SubscriptionHandler struct {
+    db  *sql.DB
+    log *logrus.Logger
+}
+
+func NewSubscriptionHandler(db *sql.DB, log *logrus.Logger) *SubscriptionHandler {
+    return &SubscriptionHandler{db: db, log: log}
+}
+
+// ===== CRUDL =====
+
+func (h *SubscriptionHandler) Create(w http.ResponseWriter, r *http.Request) {
+    // сюда вставляете код создания подписки из 3.4
+    // (с парсингом тела, валидацией, вставкой в БД)
+}
+
+func (h *SubscriptionHandler) Get(w http.ResponseWriter, r *http.Request) { ... }
+func (h *SubscriptionHandler) Update(w http.ResponseWriter, r *http.Request) { ... }
+func (h *SubscriptionHandler) Delete(w http.ResponseWriter, r *http.Request) { ... }
+func (h *SubscriptionHandler) List(w http.ResponseWriter, r *http.Request) { ... }
+
+// ===== Агрегация =====
+
+func (h *SubscriptionHandler) Aggregate(w http.ResponseWriter, r *http.Request) {
+    // сюда вставляете код агрегации из 3.4
+    // (парсинг параметров, сборка запроса, суммирование)
+}
